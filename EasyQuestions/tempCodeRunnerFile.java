a00b32cc295bf1a0b885d960ec73276d62c81a7f
mousamvishwakarma <mousamvishwakarma929@gmail.com>
2025-09-29 23:59:27 +0530

    Added Java solution for Add Binary problem (LeetCode #67)"

--- EasyQuestions/tempCodeRunnerFile.java
+++ EasyQuestions/tempCodeRunnerFile.java
@@ -1,1 +1,8 @@
-class Removeduplicate
+    public static void main(String[] args) {
+        String a = "11";
+        String b = "1";
+        String result = addBinary(a, b);
+
+        System.out.println("Sum: " + result);
+    }
+}
