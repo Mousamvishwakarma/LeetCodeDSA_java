fd2c2d86dee27d7cd7c013a7e0f948752cfce6b7
mousamvishwakarma <mousamvishwakarma929@gmail.com>
2025-09-30 23:57:08 +0530

    Add Jump Game II solution using greedy approach

--- midQuestions/tempCodeRunnerFile.java
+++ midQuestions/tempCodeRunnerFile.java
@@ -1,1 +1,6 @@
-Solution
+
+    // public static void main(String[] args) {
+    //     int[] nums = {2, 3, 1, 1, 4};
+    //     int result = jump(nums);
+    //     System.out.println("Minimum jumps to reach end: " + result);
+    // }
