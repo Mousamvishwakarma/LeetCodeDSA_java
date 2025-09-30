92fd91e4c40fa5890946c86f493c4d418d50dfaa
mousamvishwakarma <mousamvishwakarma929@gmail.com>
2025-09-30 23:58:24 +0530

    Add Jump Game II solution using greedy approach

--- midQuestions/JumpGameII.java
+++ midQuestions/JumpGameII.java
@@ -1,6 +1,6 @@
 public class JumpGameII {
-    public static int jump(int[] nums) {
-        int jumps = 0, farthest = 0, end = 0;
+    // public static int jump(int[] nums) {
+    //     int jumps = 0, farthest = 0, end = 0;
         
         for (int i = 0; i < nums.length - 1; i++) {
             farthest = Math.max(farthest, i + nums[i]);
